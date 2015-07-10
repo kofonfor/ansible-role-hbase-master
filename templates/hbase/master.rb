8f36f7d011fadecae950f49d93b190418faf9cd3
Alex Chistyakov <alexclear@gmail.com>
2015-07-10 09:32:22 +0300

    Make path and username configurable

--- templates/hbase/master.rb
+++ templates/hbase/master.rb
@@ -1,12 +1,12 @@
-Eye.application 'hbase-master' do
+Eye.application 'hbase-master-{{ env_name }}' do
   working_dir '/etc/eye'
-  stdall '/var/log/eye/hbase-master-stdall.log' # stdout,err logs for processes by default
+  stdall '/var/log/eye/hbase-master-{{ env_name }}-stdall.log' # stdout,err logs for processes by default
   trigger :flapping, times: 10, within: 1.minute, retry_in: 3.minutes
   check :cpu, every: 10.seconds, below: 100, times: 3 # global check for all processes
 
-  process :master do
-    pid_file '/var/hbase/hbase_master.pid'
-    start_command 'sudo -u hadoop /opt/hbase/bin/hbase master start'
+  process :master_{{ env_name }} do
+    pid_file '{{ hbase_var_prefix }}/hbase_master.pid'
+    start_command 'sudo -u {{ hadoop_user }} {{ hbase_distr_prefix }}/bin/hbase master start'
 
     daemonize true
     start_timeout 10.seconds
