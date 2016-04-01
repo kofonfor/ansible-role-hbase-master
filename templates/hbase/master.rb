5dbb013f9909b8b119937ee3693bf984951cf774
Alex Chistyakov <alexclear@gmail.com>
2016-04-01 18:09:26 +0300

    Use uid

--- templates/hbase/master.rb
+++ templates/hbase/master.rb
@@ -3,10 +3,11 @@ Eye.application 'hbase-master-{{ env_name }}' do
   stdall '/var/log/eye/hbase-master-{{ env_name }}-stdall.log' # stdout,err logs for processes by default
   trigger :flapping, times: 10, within: 1.minute, retry_in: 3.minutes
   check :cpu, every: 10.seconds, below: 100, times: 3 # global check for all processes
+  uid "{{ hadoop_user }}"
 
   process :master_{{ env_name }} do
     pid_file '{{ hbase_var_prefix }}/hbase_master.pid'
-    start_command 'sudo -u {{ hadoop_user }} {{ hbase_distr_prefix }}/bin/hbase master start'
+    start_command '{{ hbase_distr_prefix }}/bin/hbase master start'
 
     daemonize true
     start_timeout 10.seconds
